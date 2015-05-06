43274f29f36fd5f686509c8cc402b96b7412ffd5
Vu Pham <phvu225@gmail.com>
2015-05-06 13:58:53 +0700

    updated models

--- bot_chat_server/model/download_models.sh
+++ bot_chat_server/model/download_models.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-# add code to download the model
+wget https://dl.dropboxusercontent.com/u/45539043/bottomless_models.zip
+
+echo "========================================="
+echo "Finished downloading. Going to unzip..."
+echo "However this is a trained model on private data at Adatao, so it is not available for the public."
+echo "It will ask for a password. If you are an Adataoist, please contact @vupham to get it."
+echo "========================================="
+unzip bottomless_models.zip
